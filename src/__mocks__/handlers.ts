09f7d2e5ecbb9185a09378c2d9af764cb415d5c3
taeyoung <terry.hwang@h2ohospitality.io>
2024-11-05 17:52:17 +0900

    post 요청 추가

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,6 +1,8 @@
+import { randomUUID } from 'crypto';
+
 import { http, HttpResponse } from 'msw';
 
-import { Event } from '../types';
+import { Event, EventForm } from '../types';
 import { events } from './response/events.json' assert { type: 'json' };
 
 // ! HARD
@@ -10,7 +12,13 @@ export const handlers = [
     return HttpResponse.json({ events });
   }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const newEvent = (await request.json()) as EventForm;
+    // UUID로 하니 테스트 코드가 항상 변경되는 이슈로 UUID말고 숫자로 변경
+    events.push({ id: String(Number(events[events.length - 1].id) + 1), ...newEvent });
+
+    return HttpResponse.json({ events }, { status: 201 });
+  }),
 
   http.put('/api/events/:id', async () => {}),
 

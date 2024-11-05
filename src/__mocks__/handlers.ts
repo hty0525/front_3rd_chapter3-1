fc030d0b6f3bf609928a36bddcc61e7318dfe42d
taeyoung <terry.hwang@h2ohospitality.io>
2024-11-05 17:26:25 +0900

    msw get 구현

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -6,7 +6,9 @@ import { events } from './response/events.json' assert { type: 'json' };
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
   http.post('/api/events', async ({ request }) => {}),
 

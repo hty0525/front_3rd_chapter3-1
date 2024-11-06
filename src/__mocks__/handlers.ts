8a79ae3309b70f4075ebc36e746f989e59cb3945
terry.hwang <terry.hwang@h2ohospitality.io>
2024-11-06 23:34:54 +0900

    handlers 수정

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -13,8 +13,7 @@ export const handlers = [
   http.post('/api/events', async ({ request }) => {
     const newEvent = (await request.json()) as EventForm;
     // UUID로 하니 테스트 코드가 항상 변경되는 이슈로 UUID말고 숫자로 변경
-    events.push({ id: String(Number(events[events.length - 1].id) + 1), ...newEvent });
-
+    events.push({ id: String(events.length + 1), ...newEvent });
     return HttpResponse.json({ events }, { status: 201 });
   }),
 
@@ -23,8 +22,7 @@ export const handlers = [
     const updatedEvent = (await request.json()) as Event;
     const targetIndex = events.findIndex((event) => event.id === id);
     events[targetIndex] = updatedEvent;
-
-    return HttpResponse.json({ events: [updatedEvent] });
+    return HttpResponse.json({ events });
   }),
 
   http.delete('/api/events/:id', ({ params }) => {

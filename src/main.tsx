6cc922a2c18dd023b80741f27580e88797d95946
terry.hwang <terry.hwang@h2ohospitality.io>
2024-11-08 00:17:10 +0900

    TanstackQuery 도입

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import { TanstackQueryProvider } from './provider/TanstackQueryProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <TanstackQueryProvider>
+        <App />
+      </TanstackQueryProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
